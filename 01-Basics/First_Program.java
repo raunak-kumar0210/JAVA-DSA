16d6ee0b3facf8f65fe6da7e8119790d11361a62
Raunak Kumar <raunakjsr7016@gmail.com>
2024-10-21 12:20:40 +0530

    msg

--- 01-Basics/First_Program.java
+++ 01-Basics/First_Program.java
@@ -1,5 +1,3 @@
-package Basics;
-
 public class First_Program {
     public static void main(String args[]) {
         System.out.print("hello world");
--- 01-Basics/prime.java
+++ 01-Basics/prime.java
@@ -1,39 +1,34 @@
-package Basics;
 import java.util.*;
+
 public class prime {
-        public static void Checkprime(int num)
-        {
-            boolean isPrime = true;
-            if (num==2){
-                System.out.println(num +" is a Prime Number");
-            }
+    public static void Checkprime(int num) {
+        boolean isPrime = true;
+        if (num == 2) {
+            System.out.println(num + " is a Prime Number");
+        }
 
-            else{
-                for (int i = 2; i<= Math.sqrt(num); i++)
-                {
-                    if(num%i==0)
-                    {
-                        isPrime=false;
-                    }
-                }
-                if(isPrime==true)
-                {
-                    System.out.println(num +" is a Prime number");
-                }
-                else{
-                    System.out.println(num +" not a Prime number");
+        else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
                 }
             }
-
+            if (isPrime == true) {
+                System.out.println(num + " is a Prime number");
+            } else {
+                System.out.println(num + " not a Prime number");
+            }
         }
-        public static void main(String[] args)
-        {
-            Scanner sc =new Scanner(System.in);
-            System.out.println("Enter a number : ");
-            int num = sc.nextInt();
-            Checkprime(num);
 
-            sc.close();
-        }
-        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        Checkprime(num);
+
+        sc.close();
+    }
+
 }
